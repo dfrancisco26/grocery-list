027b2f4374eaec0ef98efb0cc0ecb5b90a6e075d
David Francisco <dfrancisco2677@gmail.com>
2022-05-23 08:18:14 -0700

    Form on list page now inserts row properly.

--- list/list.js
+++ list/list.js
@@ -1,9 +1,24 @@
-import { checkAuth, logout } from '../fetch-utils.js';
+import { checkAuth, createListItem, logout } from '../fetch-utils.js';
 
 checkAuth();
 
 const logoutButton = document.getElementById('logout');
+const form = document.getElementById('grocery-form');
+const listElem = document.getElementById('grocery-list');
+const error = document.getElementById('error');
 
 logoutButton.addEventListener('click', () => {
     logout();
+});
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const itemData = new FormData(form);
+    const data = await createListItem(itemData.get('item'), itemData.get('qty'));
+    if (data) {
+        window.location.reload(true);
+    } else {
+        error.textContent = 'Something went wrong :(';
+    }
+
 });
