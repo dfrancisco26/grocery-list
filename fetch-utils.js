cc2ff9e4aefb7f39fbd16a4e98d9075c35323b6c
David Francisco <dfrancisco2677@gmail.com>
2022-05-23 11:57:01 -0700

    More work. Still need display function for list.

--- fetch-utils.js
+++ fetch-utils.js
@@ -58,7 +58,15 @@ export async function fetchListItems() {
     }
 }
 
+export function renderItem(item) {
+    const div = document.createElement('div');
+    div.textContent = `${item.qty} ${item.name}`;
 
+    if (item.purchased) {
+        div.classList.add('complete');
+    }
+    return div;
+}
 // function checkError({ data, error }) {
 //     return error ? console.error(error) : data;
 // }
